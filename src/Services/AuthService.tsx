25c80f1ff9ae3f872cb0c4a6e43fc41997e53ab4
RapidAru <aman@iamrapid.com>
2025-09-03 10:55:46 +0530

    login fixes

--- src/Services/AuthService.tsx
+++ src/Services/AuthService.tsx
@@ -1,9 +1,9 @@
-import axios from 'axios';
-const base_url = "http://localhost:8080/auth/"
-const loginUser = async (login:any)=> {
-    return axios.post(`${base_url}login`, login)
-        .then((result:any) => result.data)
-        .catch((error:any) =>{throw error;});
+import axiosInstance from '../Interceptor/AxiosInterceptor';
+
+const loginUser = async (login: any) => {
+    return axiosInstance.post(`/auth/login`, login)
+        .then((result: any) => result.data)
+        .catch((error: any) => { throw error; });
 }
 
 export {loginUser};
